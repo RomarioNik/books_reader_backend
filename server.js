9f153050b5f60e9945e06fe3487c70776a6bc69b
RomarioNik <115925888+RomarioNik@users.noreply.github.com>
2023-07-19 16:28:41 +0100

    connect to base

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
+import mongoose from 'mongoose';
+
 import app from './app.js';
+
+const DB_HOST = "mongodb+srv://Roman:8lMGZnNx8bVkQZw7@cluster0.tkcmzd5.mongodb.net/books_reader?retryWrites=true&w=majority";
+
+mongoose.connect(DB_HOST)
+    .then(() => console.log("connect to data base"))
+    .catch((err) => console.log(err.message));
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
